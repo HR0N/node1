27092e8de501f13e8012eae658ba19ae4aeae9cf
HR0N <vivante.des@gmail.com>
2022-07-27 19:23:56 +0300

    sorry node, buy node. see you later

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ http.createServer((request, response)=>{
     console.log('server work');
     if(request.method === 'GET'){
         let urlRequest = url.parse(request.url, true);
-        console.log(urlRequest.query.test);
-        response.end('Node server work: ' + urlRequest.query.test)
+        console.log(urlRequest.query.data);
+        if(urlRequest.query.data){
+            response.end('Node server work: ' + urlRequest.query.data)
+        }else{
+            response.end('Node server work: ' + 'no data')
+        }
     }
 }).listen(3000);
+
+
+// nginx -t
+// sudo service nginx restart
